e4d67d85b0d556a5a276783811e9aaa5eccf7cab
NarcHoz95 <axk042@shsu.edu>
2019-10-20 22:25:46 -0400

    Update on shell script

--- sendIntent.sh
+++ sendIntent.sh
@@ -1,2 +1,5 @@
-adb shell 'am startservice -a com.aranteknoloji.PUSH_NOTIFICATION --es text "Here is the Push Notification"'
+adb shell 'am startservice -a com.aranteknoloji.PUSH_NOTIFICATION --es text "'$@'"'
+echo $@
+echo '--es text "'$@'"'
+#adb shell 'am startservice -a com.aranteknoloji.PUSH_NOTIFICATION --es text "Here is the Push Notification"'
 #adb shell am startservice -a com.aranteknoloji.PUSH_NOTIFICATION -e 'text' "Here' 'is' 'the' 'definition' 'of' 'kind' 'of' 'SUCCESS"
